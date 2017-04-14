a3b5ed20b6a8a94cb79b6a4c0f845d3756d86c67
@Pikhota <s.pikhota@gmail.com>
2017-04-14 12:19:42 +0300

    update

--- Pacman/MainWindow.xaml.cs
+++ Pacman/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.Windows;
 using System.Windows.Input;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
@@ -13,149 +14,209 @@ namespace Pacman
     /// </summary>
     public partial class MainWindow : Window
     {
-
-        public Pac Pac = new Pac();
+        private const double Speed = 0;
+        private const double Step = 5;
+        public Rect Pacman;
+        public TranslateTransform Trans = new TranslateTransform();
         public DoubleAnimation AnimationX = new DoubleAnimation();
         public DoubleAnimation AnimationY = new DoubleAnimation();
-        public RectangleGeometry GeometryPac = new RectangleGeometry();
-        private const double Step = 5;
+        public string SideCollision;
+        public int LifeCount = 100;
 
         public MainWindow()
         {
-
             InitializeComponent();
-
             KeyDown += Moving;
+            Pacman = new Rect(0, 0, ImgPacman.Width, ImgPacman.Height);
+            ImgPacman.RenderTransform = Trans;
 
-            #region Создание пакмена
-            Pac.PacPath.Stroke = Brushes.Yellow;
-            Pac.PacPath.StrokeThickness = 1;
-            Pac.PacPath.Fill = Pac.SolidColorBrush;
-            GeometryPac.RadiusX = 26;
-            GeometryPac.RadiusY = 26;
-            GeometryPac.Rect = Pac.Pacman;
-            Pac.PacPath.Data = GeometryPac;
-            Win.Children.Add(Pac.PacPath);
-            #endregion
-            Pac.PacPath.RenderTransform = Pac.Trans;
         }
 
         #region Проверка на столкновение со стеной
+
         private bool CollisionWall()
         {
             var flag = false;
-            Rectangle []walls = new Rectangle[]{ WallA1, WallA2, WallA3, WallA4,WallA5,WallA6,WallA7,WallA8,WallC1,WallC2,WallC3
+            var walls = new[]{ WallA1, WallA2, WallA3, WallA4,WallA5,WallA6,WallA7,WallA8,WallC1,WallC2,WallC3
             , WallM1,WallM2,WallM3,WallM4,WallM5,WallN1,WallN2,WallN3,WallP1,WallP2,WallP3,WallP4};
-            foreach(var wall in walls)
+            foreach (var wall in walls)
             {
-                Rect rect = GetWall(wall);
-                if(Pac.Pacman.IntersectsWith(rect))
+                var rectWall = GetRect(wall);
+                if (Pacman.IntersectsWith(rectWall))
                 {
                     flag = true;
                 }
             }
-            return flag;            
+            return flag;
+        }
+
+        private bool CollisionGhost()
+        {
+            var flag = false;
+            var ghosts = new[] { ImgGhostWhite, ImgGhostRed, ImgGhostYellow, ImgGhostViolet };
+            foreach (var ghost in ghosts)
+            {
+                var rectGhost = GetRect(ghost);
+                if (Pacman.IntersectsWith(rectGhost))
+                {
+                    flag = true;
+                }
+            }
+            return flag;
         }
         #endregion
+
+
 
         #region Движение пакмена по событию (нажатие на клавиши - стрелочки)
         private void Moving(object sender, KeyEventArgs e)
         {
+
             switch (e.Key)
             {
                 case Key.Right:
-                    Pac.Pacman.X = Pac.Trans.X;
-                    if (Pac.Pacman.X < Win.Width - Pac.Pacman.Width - GeometryPac.RadiusX / 2 - Step)
+                    CurrentLocation(e);
+                    if (Pacman.Right < Win.Width - Step * 2)
                     {
-                        if (CollisionWall())
+                        
+                        if (CollisionWall() && SideCollision != "left")
                         {
-                            AnimationX = new DoubleAnimation(Pac.Pacman.X - Step, TimeSpan.FromMilliseconds(Pac.Speed));
-                            Pac.Trans.BeginAnimation(TranslateTransform.XProperty, AnimationX);
-                            AnimateY();
+                            SideCollision = "right";
+                            if (SideCollision == "right")
+                            {
+                                while (CollisionWall())
+                                {
+                                    AnimationX = new DoubleAnimation(Pacman.X--, TimeSpan.FromMilliseconds(Speed));
+                                    Trans.BeginAnimation(TranslateTransform.XProperty, AnimationX);
+                                    AnimateY();
+                                }
+                            }
                         }
                         else
                         {
-                            AnimationX = new DoubleAnimation(Pac.Pacman.X + Step, TimeSpan.FromMilliseconds(Pac.Speed));
-                            Pac.Trans.BeginAnimation(TranslateTransform.XProperty, AnimationX);
+                            AnimationX = new DoubleAnimation(Pacman.X + Step, TimeSpan.FromMilliseconds(Speed));
+                            Trans.BeginAnimation(TranslateTransform.XProperty, AnimationX);
                             AnimateY();
+                            SideCollision = string.Empty;
                         }
                     }
                     break;
                 case Key.Left:
-                    Pac.Pacman.X = Pac.Trans.X;
-                    if (Pac.Pacman.X > 0)
+                    CurrentLocation(e);
+                    if (Pacman.Left > 0)
                     {
-                        if (CollisionWall())
+
+                        if (CollisionWall() && SideCollision !="right")
                         {
-                            AnimationX = new DoubleAnimation(Pac.Pacman.X + Step, TimeSpan.FromMilliseconds(Pac.Speed));
-                            Pac.Trans.BeginAnimation(TranslateTransform.XProperty, AnimationX);
-                            AnimateY();
+                            SideCollision = "left";
+                            if (SideCollision == "left")
+                            {
+                                while (CollisionWall())
+                                {
+                                    AnimationX = new DoubleAnimation(Pacman.X++, TimeSpan.FromMilliseconds(Speed));
+                                    Trans.BeginAnimation(TranslateTransform.XProperty, AnimationX);
+                                    AnimateY();
+                                }
+                            }
                         }
                         else
                         {
-                            AnimationX = new DoubleAnimation(Pac.Pacman.X - Step, TimeSpan.FromMilliseconds(Pac.Speed));
-                            Pac.Trans.BeginAnimation(TranslateTransform.XProperty, AnimationX);
+                            AnimationX = new DoubleAnimation(Pacman.X - Step, TimeSpan.FromMilliseconds(Speed));
+                            Trans.BeginAnimation(TranslateTransform.XProperty, AnimationX);
                             AnimateY();
+                            SideCollision = string.Empty;
                         }
                     }
                     break;
                 case Key.Up:
-                    Pac.Pacman.Y = Pac.Trans.Y;
-                    if (Pac.Pacman.Y > 0)
+                    CurrentLocation(e);
+                    if (Pacman.Top > 0)
                     {
-                        if (CollisionWall())
+                        if (CollisionWall() && SideCollision != "bottom")
                         {
-                            AnimateX();
-                            AnimationY = new DoubleAnimation(Pac.Pacman.Y + Step, TimeSpan.FromMilliseconds(Pac.Speed));
-                            Pac.Trans.BeginAnimation(TranslateTransform.YProperty, AnimationY);
+                            SideCollision = "top";
+                            if (SideCollision == "top")
+                            {
+                                while (CollisionWall())
+                                {
+                                    AnimateX();
+                                    AnimationY = new DoubleAnimation(Pacman.Y++, TimeSpan.FromMilliseconds(Speed));
+                                    Trans.BeginAnimation(TranslateTransform.YProperty, AnimationY);
+                                }
+                            }
                         }
                         else
                         {
-                            AnimationX = new DoubleAnimation(Pac.Trans.X, TimeSpan.FromMilliseconds(Pac.Speed));
-                            Pac.Trans.BeginAnimation(TranslateTransform.XProperty, AnimationX);
-                            AnimationY = new DoubleAnimation(Pac.Pacman.Y - Step, TimeSpan.FromMilliseconds(Pac.Speed));
-                            Pac.Trans.BeginAnimation(TranslateTransform.YProperty, AnimationY);
+                            AnimateX();
+                            AnimationY = new DoubleAnimation(Pacman.Y - Step, TimeSpan.FromMilliseconds(Speed));
+                            Trans.BeginAnimation(TranslateTransform.YProperty, AnimationY);
+                            SideCollision = string.Empty;
                         }
+
                     }
                     break;
                 case Key.Down:
-                    Pac.Pacman.Y = Pac.Trans.Y;
-                    if (Pac.Pacman.Y < Win.Height - Pac.Pacman.Height - GeometryPac.RadiusX / 2 - Step)
+                    CurrentLocation(e);
+                    if (Pacman.Bottom < Win.Height - Step * 2)
                     {
-                        if (CollisionWall())
+                        if (CollisionWall() && SideCollision != "top")
                         {
-                            AnimateX();
-                            AnimationY = new DoubleAnimation(Pac.Pacman.Y - Step, TimeSpan.FromMilliseconds(Pac.Speed));
-                            Pac.Trans.BeginAnimation(TranslateTransform.YProperty, AnimationY);
+                            SideCollision = "bottom";
+                            if (SideCollision == "bottom")
+                                while (CollisionWall())
+                                {
+                                    AnimateX();
+                                    AnimationY = new DoubleAnimation(Pacman.Y--, TimeSpan.FromMilliseconds(Speed));
+                                    Trans.BeginAnimation(TranslateTransform.YProperty, AnimationY);
+                                }
                         }
                         else
                         {
                             AnimateX();
-                            AnimationY = new DoubleAnimation(Pac.Pacman.Y + Step, TimeSpan.FromMilliseconds(Pac.Speed));
-                            Pac.Trans.BeginAnimation(TranslateTransform.YProperty, AnimationY);
+                            AnimationY = new DoubleAnimation(Pacman.Y + Step, TimeSpan.FromMilliseconds(Speed));
+                            Trans.BeginAnimation(TranslateTransform.YProperty, AnimationY);
+                            SideCollision = string.Empty;
                         }
                     }
                     break;
+            }
+            if (CollisionGhost())
+            {
+                Life.Content = LifeCount--;
             }
         }
         #endregion
         #region Вспомогающие методы (уменьшение дублированого кода и т.п.)
         private void AnimateX()
         {
-            AnimationX = new DoubleAnimation(Pac.Trans.X, TimeSpan.FromMilliseconds(Pac.Speed));
-            Pac.Trans.BeginAnimation(TranslateTransform.XProperty, AnimationX);
+            AnimationX = new DoubleAnimation(Pacman.X, TimeSpan.FromMilliseconds(Speed));
+            Trans.BeginAnimation(TranslateTransform.XProperty, AnimationX);
         }
 
         private void AnimateY()
         {
-            AnimationY = new DoubleAnimation(Pac.Trans.Y, TimeSpan.FromMilliseconds(Pac.Speed));
-            Pac.Trans.BeginAnimation(TranslateTransform.YProperty, AnimationY);
+            AnimationY = new DoubleAnimation(Pacman.Y, TimeSpan.FromMilliseconds(Speed));
+            Trans.BeginAnimation(TranslateTransform.YProperty, AnimationY);
         }
 
-        private Rect GetWall(Rectangle wall)
+        private Rect GetRect(Rectangle wall)
         {
-            GeneralTransform TransWall = wall.TransformToVisual(this);
-            return TransWall.TransformBounds(new Rect(new Size(wall.ActualWidth, wall.ActualHeight)));
+            var transWall = wall.TransformToVisual(this);
+            return transWall.TransformBounds(new Rect(new Size(wall.ActualWidth, wall.ActualHeight)));
+        }
+
+        private Rect GetRect(Image ghost)
+        {
+            var transGhost = ghost.TransformToVisual(this);
+            return transGhost.TransformBounds(new Rect(new Size(ghost.ActualWidth, ghost.ActualHeight)));
+        }
+
+        private void CurrentLocation(KeyEventArgs e)
+        {
+            if (Key.Right == e.Key || Key.Left == e.Key)
+                Pacman.X = Trans.X;
+            if (Key.Up == e.Key || Key.Down == e.Key)
+                Pacman.Y = Trans.Y;
         }
         #endregion
     }
